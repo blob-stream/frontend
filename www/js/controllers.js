4601c546d2a79d59fd5a4296de64513d27cf938d
Darja Ferber <df029>
2016-06-20 22:14:57 +0200

    doing some stuff, such as order by positioning

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('app.controllers', [])
    
 
 .controller('addABlobCtrl', function($scope) {
-
+	$scope.name = {};
 })
     
 .controller("blobStreamCtrl", function($scope, $ionicLoading){
@@ -15,23 +15,32 @@ angular.module('app.controllers', [])
 			heading: "Feeling good",
 			description: "This is a wonderful day. The weather is fine and I go swimming tonight. =)",
 			image: "src/test.jpg",
-			creator: "Doreen"
+			creator: "Doreen",
+			position: 4
 		},
 		{
 			heading: "Sleepy right now",
 			description: "It is really cool when your alarm clock wears footy pyjamas.",
-			creator: "Dary"
+			creator: "Dary",
+			position: 3
 		},
 		{
 			heading: "Grass is always greener on the other side.",
-			creator: "Dary"
+			creator: "Dary",
+			position: 2
 		},
 		{
 			heading: "Yaay, marriage is coming soon.",
 			creator: "Meli",
-			image: "src/meli.jpg"
+			image: "src/meli.jpg",
+			position: 1
+
 		}
 	];
+
+	$scope.coolIt = function($scope, $item){
+
+	}
 
 	$scope.download = function() {
 	    $ionicLoading.show({
