779cfab449bca223f8d6f748b601d3cf03929050
Darja Ferber <df029@hdm-stuttgart.de>
2016-07-03 21:23:50 +0200

    Adding blob directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,13 @@
 angular.module('app.directives', [])
 
-.directive('blankDirective', [function(){
+/*.directive('blankDirective', [function(){
 
+}]);*/
+
+.directive('streamBlob', [function(){
+	return{
+		restrict: 'E',
+		templateUrl: '../templates/blob.html',
+		controller: 'blobStreamCtrl'
+	};
 }]);
-
