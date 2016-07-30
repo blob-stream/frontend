84f9c6463342f9743f39242ee7676b2b19bc1c7b
Darja Ferber <df029@hdm-stuttgart.de>
2016-07-30 23:00:17 +0200

    Blobbing images working (offline), transfering insto base64

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['naif.base64'])
   
 .controller('pickANameCtrl', function($scope, UserService) {
 	this.userName = "";
@@ -8,12 +8,27 @@ angular.module('app.controllers', [])
 	};
 })
 
-.controller('addABlobCtrl', function($scope, UserService, BlobService) {
+.controller('addABlobCtrl', function($scope, UserService, BlobService, $http, $window, $rootScope) { 
 	this.userName = UserService.getUserName();
+
+	// Using naif.base64 stuff, see https://github.com/adonespitogo/angular-base64-upload for more details
+	$scope.onChange = function (e, fileList) {
+		console.log("changing image...")
+	};
+
+	$scope.onLoad = function (e, reader, file, fileList, fileOjects, fileObj) {
+		console.log("loading image...");
+	};
+
+	var uploadedCount = 0;
+	$scope.files = [];
+	//
 
 	this.addNewBlob = function(newblob){
 		newblob.voting = 0;
 		newblob.creator = this.userName;
+		newblob.dataURL = "data:" + newblob.dataURL.filetype + ";base64," + newblob.dataURL.base64;
+		console.log(newblob.dataURL);
 		BlobService.addNewBlob(newblob);
 	}
 })
